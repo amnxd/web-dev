5130bd2394b3975b8d974cb7db24f49f97305a99
Aman <bhaskarshah4235@gmail.com>
2025-04-19 21:30:50 +0530

    aksjbda

--- js/vanilajs/script.js
+++ js/vanilajs/script.js
@@ -4,14 +4,18 @@
 // function myFun() {
 //   return "Hoisting Works"
 // }
-function outer(){
-  let count = 0;
-  return function inner() {
-    count++;
-    console.log(count);
-  };
-}
 
-const counter = outer ();
-counter();
-counter();
+// function outer(){
+//   let count = 0;
+//   return function inner() {
+//     count++;
+//     console.log(count);
+//   };
+// }
+
+// const counter = outer ();
+// counter();
+// counter();
+
+
+
