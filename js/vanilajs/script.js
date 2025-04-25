4578dcc9558f6ac67494667a39489d429e571a56
Aman <bhaskarshah4235@gmail.com>
2025-04-25 09:03:56 +0530

    lec23

--- js/vanilajs/script.js
+++ js/vanilajs/script.js
@@ -19,3 +19,32 @@
 
 
 
+// // array -1 to initialize
+// var arr = []; //if size is not defined
+// var arr2 = new Array(30);  //if size is fixed/defined
+
+
+
+// // //OBJECT
+// var obj = {
+//   name: 'Aman',
+//   age: 99,
+//   class: 'ccml'
+// };
+
+// var obj2 = new Object();
+
+// for ( let i = 1; i<=3; i++){
+//   for(let j=1; j<=4; j++){
+//     console.log("*");
+//   }
+// }
+
+
+// let str = 'JS';
+// for(let char of str){
+//   console.log(char+char);
+// }
+
+
+var arr = [3, 1, 4, 3, 1, 4, 2, 5] 
