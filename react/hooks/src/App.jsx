cec8d236df7175af1e7c67cedec0f3829d65e36c
amnxd <bhaskarshah4235@gmail.com>
2026-04-25 23:55:41 +0530

    use state

--- react/hooks/src/App.jsx
+++ react/hooks/src/App.jsx
@@ -1,29 +1,46 @@
 import React from 'react'
+import { useState } from 'react';
+
+// const App = () => {
+//   const profiles = [
+//     { name : "Aman", age : 20},
+//     { name : "Rahul", age : 19},
+//     { name : "Kamlesh", age : 18},
+//   ];
+
+//   const updatedProfiles = profiles.map((profile, index)=> {
+//     // console.log(profile, index);
+//     return (
+//       <li key={index}>
+//         <span>Name : {profile.name}</span> |
+//         <small> Age : {profile.age}</small>
+//       </li>
+//       )
+//   });
+
+//   return (
+//     <div>
+//       <h1>Rendering JSON</h1>
+//       <ol>{updatedProfiles}</ol>
+//     </div>
+//   )
+// }
 
 const App = () => {
-  const profiles = [
-    { name : "Aman", age : 20},
-    { name : "Rahul", age : 19},
-    { name : "Kamlesh", age : 18},
-  ];
+  const [username, setUser] = useState("Aman");
 
-  const updatedProfiles = profiles.map((profile, index)=> {
-    // console.log(profile, index);
-    return (
-      <li key={index}>
-        <span>Name : {profile.name}</span> |
-        <small> Age : {profile.age}</small>
-      </li>
-      )
-  });
-
+  const changeHandler = () => {
+    setUser("Rahul");
+  }
+  console.log(username);
   return (
-    <div>
-      <h1>Rendering JSON</h1>
-      <ol>{updatedProfiles}</ol>
-    </div>
+    <>
+      <h1>Username</h1>
+      <h2>{username}</h2>
+      <button onClick={changeHandler}>change</button>
+    </>
   )
 }
 
 
-export default App
+export default App;
